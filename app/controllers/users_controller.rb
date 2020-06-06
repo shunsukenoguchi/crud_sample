41a1158330281f763a5f876d7c52a15a106915a7
Shunsuke Noguchi <bakutensanba89@yahoo.ne.jp>
2020-06-06 16:01:03 +0900

    resourcesを使ったCRUDアプリに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  def show
+    @user = User.find(params[:id])
+  end
   def update
     user = User.find(params[:id])
     user.update(user_params)
